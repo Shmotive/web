f9c897842e3c1f0556e70d69c1c611352d0d55df
iangu48 <iangu48@users.noreply.github.com>
2023-12-13 04:57:17 +0000

    deploy: 5e7ab5cdcf6ec261b2756be3e042839ca3059690

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('dummy test', () => {
-  render(<App />);
-  const linkElement = screen.getByText('login');
-  expect(linkElement).toBeInTheDocument();
-});
