5203b3b6f61aa5f51d9182cd236e7bd579ca1433
Ian Gu <iangu@Ians-MacBook-Pro.local>
2023-12-12 23:15:10 -0500

    test: add dummy test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('dummy test', () => {
+  render(<App />);
+  const linkElement = screen.getByText('login');
+  expect(linkElement).toBeInTheDocument();
+});
