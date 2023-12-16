b91bf5e66e596f42572f4b6b75e354bfcf407c0f
kshmakov1 <kshmakov@ryerson.ca>
2023-12-16 16:21:09 -0500

    Added placeholder member list for lobby
    Added a member list to lobby as a placeholder for when we implement graphql

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -1,28 +1,46 @@
-import { Link, useLoaderData, useLocation } from "react-router-dom"
+import { Link, useLoaderData, useLocation } from "react-router-dom";
 import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
+import React, {useState} from "react";
 import "../assets/css/lobby.css";
- 
+
 
 export default function LobbyPage() {
     const location = useLocation();
     const passedState = location.state;
+	// const joinedMembersArray = ['Member 1', 'Member 2', 'Member 3']; 
+
+	const [joinedMembersArray, setJoinedMembersArray] = useState(['Member 1', 'Member 2', 'Member 3']); // Initialize with existing members
+
+    const handleAddMember = () => {
+        const newMember = `New Member ${joinedMembersArray.length + 1}`;
+        setJoinedMembersArray([...joinedMembersArray, newMember]);
+    };
 
     console.log(passedState)
 
     return <>
-    <div className="container">
-        <Image className="logo" src={require("../assets/shrug-smiley.jpg")} />
-        <div className="lobby-code">
-            lobby page for {passedState.code}
-        </div>
-    <Link to="/">home</Link>
-        
-            <Button className="ready-button" variant="secondary" size="lg"> waiting for host... </Button>
-    </div>
-        <div className="lobby">
-            lobby page for {passedState.guestName}
-        </div>
+		<div className="container">
+			<Image className="logo" src={require("../assets/shrug-smiley.jpg")} />
+			<div className="lobby-code">
+				lobby page for {passedState.code}
+				<div className="joined-members-section">
+                    <ul>
+                        {joinedMembersArray.map((member, index) => (
+                            <li key={index}>{member}</li>
+                        ))}
+                    </ul>
+					<button onClick={handleAddMember}>Add Member</button>
+
+                </div>
+			</div>
+			<Link to="/">home</Link>
+			
+			<Button className="ready-button" variant="secondary" size="lg"> waiting for host... </Button>
+		</div>
+		<div className="lobby">
+			lobby page for {passedState.guestName}
+		</div>
     </>
 }
 
