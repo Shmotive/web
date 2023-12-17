4e6f2ffc934fd37de9167cdcdb4d9b5144ecaec8
Cristian <cristiansolomon100@gmail.com>
2023-12-16 19:09:18 -0500

    feat(KAN-27): add proper flex-box styling for everything on the lobby page. Add pseudo-code for functionality of button depending on whether user is host. pseudocode placeholders for graphql listeners and queries.

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -1,4 +1,4 @@
-import { Link, useLoaderData, useLocation } from "react-router-dom";
+import { Link, useLoaderData, useLocation, useNavigate } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import React, {useState, useRef} from "react";
@@ -9,44 +9,54 @@ export default function LobbyPage() {
     const location = useLocation();
     const passedState = location.state;
 	const listRef = useRef(null);
+    const navigate = useNavigate();
 	// const joinedMembersArray = ['Member 1', 'Member 2', 'Member 3']; 
 
 	const [joinedMembersArray, setJoinedMembersArray] = useState(['Member 1', 'Member 2', 'Member 3']); // Initialize with existing members
 
-    const handleAddMember = () => {
-        const newMember = `New Member ${joinedMembersArray.length + 1}`;
-        setJoinedMembersArray(prevMembers => [...prevMembers, newMember]);
 
-        // Scroll to the bottom after adding a member
-        if (listRef.current) {
-            listRef.current.scrollTop = listRef.current.scrollHeight;
-        }
-    };
-
+    // const buttonOnClick = () => {
+    //     navigate('/recommendations/' + passedState.code)
+    // };
 	
     console.log(passedState)
 
     return <>
-		<div className="container">
-			<Image className="logo" src={require("../assets/shrug-smiley.jpg")} />
-			<div className="lobby-code">
-				lobby page for {passedState.code}
-				<div className="joined-members-section">
-                    <ul className="members-list">
-                        {joinedMembersArray.map((member, index) => (
-                            <li key={index}>{member}</li>
-                        ))}
-                    </ul>
+            <div className="container">
+                <div className="col-left">
+                    <Image className="logo" src={require("../assets/shrug-smiley.jpg")} />
+                    <div className="joined-members-container">
+                            {joinedMembersArray.map((member, index) => (
+                                <UserSlot key={index} name={member} /> 
+                            ))}
+                    </div>
+                </div>
+                <div className="col-middle">
+                    <div className="flex-1"></div>
+                    <div className="lobby-code">
+                        lobby page for {passedState.code}
+                    </div>
+                    <div className="button-container">
+                    <Button disabled={true} 
+                        className="ready-button user-view" 
+                        variant="secondary"
+                        size="lg" 
+                        >{(false) ? "start" : "waiting for host..."}  
+                    </Button>
+                        {/* above, the check for whether the user viewing is host
+                        is done by comparing the current user id to that of the host
+                        on the DB i.e. (user.id === host.id) ? 'start' : 'waiting for host...')  */}
+                    </div>
+                </div>
+                <div className="col-right">
+                    <Link to="/">home</Link>
                 </div>
 			</div>
-			<Link to="/">home</Link>
-			
-			<Button className="ready-button" variant="secondary" size="lg"> waiting for host... </Button>
-		</div>
-		<div className="lobby">
-			lobby page for {passedState.guestName}
-		</div>
     </>
+}
+
+const UserSlot = ({ name, logo }) => {
+    return <div className="members"> {name}</div>
 }
 
 // notice in App.js we have path /lobby/:id
